f0b661a5567d0230c8bd42eec2e6e4a8654df4c5
Yashirmare <csherwin235@googlemail.com>
2015-06-25 17:44:11 +0100

    Fucked up the price

--- lua/ttt_upgrades/misc/drow_ranger.lua
+++ lua/ttt_upgrades/misc/drow_ranger.lua
@@ -11,7 +11,6 @@ EVENT.Hooks = { "EntityTakeDamage" }
 function EVENT:EntityTakeDamage(ent, dmginfo)
 	local attacker = dmginfo:GetAttacker()
 	if GetRoundState() == ROUND_ACTIVE and ent:IsPlayer() and ent:IsActive() and dmginfo:IsDamageType(DMG_BULLET) and attacker:UpgradeEnabled("drow_ranger") then
-		attacker:SetHealth(math.Clamp(attacker:Health() + math.Clamp(math.floor(damages/10), 0, 10), 0, 100))
 		if timer.Exists(timername) then
 			timer.Remove(timername)
 		end
